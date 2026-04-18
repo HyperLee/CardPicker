c9485f4809cec34c63b8f8a36ee7bd2e605b446a
Peter.Chiu <tidub76217@hotmail.com>
2026-04-18 23:47:36 +0800

    fix: 更新組件資訊版本號與快取內容

--- CardPicker/obj/Debug/net10.0/CardPicker.AssemblyInfo.cs
+++ CardPicker/obj/Debug/net10.0/CardPicker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CardPicker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b62a9ef57bd146f6b0e0cdfdef3a8004cff8a74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7bbb705bd5a4f49494855e7aea85b4a4cc6cb06")]
 [assembly: System.Reflection.AssemblyProductAttribute("CardPicker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CardPicker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
